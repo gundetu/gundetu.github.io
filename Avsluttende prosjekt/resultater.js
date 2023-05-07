0790a414b52b6d5c7fecd3cdc664b4adbca68807
gundetu <131137196+gundetu@users.noreply.github.com>
2023-05-07 21:56:01 +0200

    Add files via upload

---
+++ Avsluttende prosjekt/resultater.js
@@ -0,0 +1,10 @@
+let quizEl = document.getElementById('Quiz')
+let karttegnEl = document.getElementById('karttegn')
+let memoryEl = document.getElementById('memory')
+
+quizEl.innerHTML = localStorage.highscore + " poeng"
+karttegnEl.innerHTML = localStorage.best + " i streak "
+memoryEl.innerHTML = localStorage.trekk + " trekk "
+
+
+
