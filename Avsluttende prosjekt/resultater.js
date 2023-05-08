f6ad744dea20faf8380521f0d1c864dac4e47022
gundetu <131137196+gundetu@users.noreply.github.com>
2023-05-08 22:40:49 +0200

    Update resultater.js
    Signed-off-by: gundetu <131137196+gundetu@users.noreply.github.com>

--- Avsluttende prosjekt/resultater.js
+++ Avsluttende prosjekt/resultater.js
@@ -2,26 +2,7 @@ let quizEl = document.getElementById('Quiz')
 let karttegnEl = document.getElementById('karttegn')
 let memoryEl = document.getElementById('memory')
 
-if(!localStorage.highscore ){
-    quizEl.innerHTML = "0 poeng"
-}
-else{
-    quizEl.innerHTML = localStorage.highscore + " poeng"
-}
-
-
-if(!localStorage.best){
-    karttegnEl.innerHTML = " 0 i streak "
-}
-else{
-    karttegnEl.innerHTML = localStorage.best + " i streak "
-}
-
-if(!localStorage.trekk){
-    memoryEl.innerHTML = "ikke prøvd"
-}
-else{
-    memoryEl.innerHTML = localStorage.trekk + " trekk"
-}
-
+quizEl.innerHTML = localStorage.highscore + " poeng"
+karttegnEl.innerHTML = localStorage.best + " i streak "
+memoryEl.innerHTML = localStorage.trekk + " trekk"
 
